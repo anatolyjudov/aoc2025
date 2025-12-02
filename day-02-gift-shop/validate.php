5739e9cda6c40b4a287eccb5e5b4e1c67b6dae12
anatolyjudov <anatoly.judov@gmail.com>
2025-12-02 16:40:08 +0200

    day 2: gift shop

---
+++ day-02-gift-shop/validate.php
@@ -0,0 +1,32 @@
+<?php
+
+$ranges = array_map(
+    fn($range) => explode('-', $range),
+    explode(',', trim(file_get_contents("input.txt")))
+);
+
+echo
+    array_reduce($ranges, fn($sum, $range) => $sum += calculate($range[0], $range[1])) . PHP_EOL;
+
+function calculate($begin, $end): int
+{
+    if (strlen($begin) === strlen($end) - 1) {
+        $border = 10 ** strlen($begin);
+        return calculate($begin, $border - 1) + calculate($border, $end);
+    }
+
+    $sum = 0;
+    $len = strlen($begin);
+
+    if ($len % 2 === 0) {
+        $splitSize = $len >> 1;
+        $split = substr($begin, 0, $splitSize);
+
+        while(($can = str_repeat($split, intdiv($len, $splitSize))) <= $end) {
+            if ($can >= $begin) $sum += $can;
+            $split++;
+        }
+    }
+
+    return $sum;
+}
